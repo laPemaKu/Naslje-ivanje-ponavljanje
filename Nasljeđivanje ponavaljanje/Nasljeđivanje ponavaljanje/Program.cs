2900d93430bdf2cb7df631e44488708b3adda573
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2022-11-16 09:30:43 +0100

    Add files via upload

---
+++ Nasljeđivanje ponavaljanje/Nasljeđivanje ponavaljanje/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje_ponavaljanje
+{
+    internal class Program
+    {
+        class GeometrijskiLik
+        {
+            int brojStranica;
+            double[] stranice= new double[10];
+            
+            public void UcitajStranice()
+            {
+                for(int i=0;i<this.brojStranica; i++)
+                {
+                    Console.WriteLine("Stranica {0}: ",i+1);
+                    string upis = Console.ReadLine();
+                    stranice[i]= Convert.ToDouble(upis);
+                }
+            }
+            public double Opseg()
+            {
+                double opseg=0;
+                for(int i=0;i<this.brojStranica; i++)
+                {
+                    opseg += stranice[i];
+                }
+                return opseg;
+            }
+            public GeometrijskiLik()
+            {
+
+            }
+            public GeometrijskiLik(int brojStranica)
+            {
+                this.brojStranica = brojStranica;
+            }
+            
+        }
+        class Trokut : GeometrijskiLik
+        {
+            public Trokut() : base(3)
+            {
+
+            }
+        }
+        class Cetvertokut : GeometrijskiLik
+        {
+            public Cetvertokut () : base(4)
+            {
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Trokut trokut = new Trokut();
+            Cetvertokut cetvertokut = new Cetvertokut();
+
+            Console.ReadLine(trokut.UcitajStranice());
+            Console.WriteLine(trokut.Opseg());
+
+            Console.ReadLine(trokut.UcitajStranice());
+            Console.WriteLine(cetvertokut.Opseg());
+
+            Console.ReadKey();
+        }
+    }
+}
